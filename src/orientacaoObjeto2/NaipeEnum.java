50f859002bba64a627f011c79a8750ec3c1f7c0c
Andressa dos Santos Pereira <andressasantos@ibm.com>
2021-10-22 13:59:28 -0300

    enum

---
+++ src/orientacaoObjeto2/NaipeEnum.java
@@ -0,0 +1,22 @@
+package orientacaoObjeto2;
+
+public enum NaipeEnum {
+
+	OURO("Vermelho"), COPAS("Preto"), PAUS("Preto"), ESPADA("Vermelho");
+	
+	private String cor;
+	
+	NaipeEnum(String cor){
+		this.cor= cor;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	
+}
---
+++ src/orientacaoObjeto2/TesteEnum.java
@@ -0,0 +1,9 @@
+package orientacaoObjeto2;
+
+public class TesteEnum {
+	
+	public static void main(String[] args) {
+		Carta quatroPaus= new Carta(2, NaipeEnum.PAUS);
+		quatroPaus.imprirmirCarta();
+	}
+}
