873117c107023019826c633a7352382feb3b162a
Andressa <47118954+santosandressa@users.noreply.github.com>
2021-11-03 11:10:33 -0300

    arquivo properties

---
+++ src/arquivoproperties/LendoArquivoProperties.java
@@ -0,0 +1,23 @@
+package arquivoproperties;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class LendoArquivoProperties {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		Properties prop= new Properties();
+		prop.load(new FileInputStream("./config/config.properties"));
+		
+		String url= prop.getProperty("banco,dados.url");
+		String usuario= prop.getProperty("banco.dados.usuario");
+		String senha= prop.getProperty("banco.dados.senha");
+		
+		
+	}
+
+}
